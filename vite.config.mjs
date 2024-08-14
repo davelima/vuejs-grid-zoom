4b0392908ea47697bf4c2362f1c28f40fe137319
David Lima <dave@davidlima.dev>
2024-08-14 17:11:18 -0300

    Update .d.ts file generation

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,20 +1,21 @@
 import {defineConfig} from "vite";
 import {resolve} from "path";
 import vue from "@vitejs/plugin-vue";
-import dts from "vite-plugin-dts";
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts({rollupTypes: true})],
+  plugins: [vue()],
   build: {
     lib: {
       // src/indext.ts is where we have exported the component(s)
       entry: resolve(__dirname, "src/index.ts"),
       name: "VueJsGridZoom",
       // the name of the output files when the build is run
-      fileName: "vuejs-grid-zoom",
+      fileName: "index"
     },
     rollupOptions: {
+      plugins: [dts({rollupTypes: true, entryRoot: 'src/'})],
       // make sure to externalize deps that shouldn't be bundled
       // into your library
       external: ["vue"],
